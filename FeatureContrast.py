db83a38af378c59f1461106134e520ec26f65400
blackeyy <zaixiadouing@qq.com>
2022-08-09 15:05:16 +0800

    图片相似度比较

--- FeatureContrast.py
+++ FeatureContrast.py
@@ -18,72 +18,6 @@ from PIL import Image
 from torchvision import transforms
 from tqdm import tqdm
 
-# class FaceVsimilarity():
-#
-#     def __init__(self, detector_path, face_rec_model_path, faces_folder_path, TestImg):
-#         # 人脸关键点检测器
-#         self.predictor_path = detector_path
-#         # 人脸识别模型、提取特征值
-#         self.face_rec_model_path = face_rec_model_path
-#         # 训练图像文件夹
-#         self.faces_folder_path = faces_folder_path
-#
-#         # 测试数据
-#         self.TestImg = TestImg
-#
-#     def run(self):
-#         # 加载模型
-#         # detector = dlib.get_frontal_face_detector()
-#         # sp = dlib.shape_predictor(self.predictor_path)
-#         facerec = dlib.face_recognition_model_v1(self.face_rec_model_path)
-#
-#         candidate = []  # 存放训练集人物名字
-#         descriptors = []  # 存放训练集图片特征列表
-#
-#         for f in glob.glob(os.path.join(self.faces_folder_path, "*.jpg")):
-#             print("正在处理: {}".format(f))
-#             img = io.imread(f)
-#             candidate.append(f.split('\\')[-1].split('.')[0])
-#             # 人脸检测
-#             # dets = detector(img, 1)
-#             for k, d in enumerate(dets):
-#                 # shape = sp(img, d)
-#                 # 提取特征
-#                 face_descriptor = facerec.compute_face_descriptor(img, shape)
-#                 v = numpy.array(face_descriptor)
-#                 descriptors.append(v)
-#
-#         print('计算图片特征，并放到一个列表里面:识别训练完毕！')
-#
-#         try:
-#             # test_path=input('请输入要检测的图片的路径（记得加后缀哦）:')
-#             img = io.imread(self.TestImg)
-#             dets = detector(img, 1)
-#         except:
-#             print('输入路径有误，请检查！')
-#
-#         dist = []
-#         global Range;   Range = 0  # 测试人脸与训练数据之间的欧式距离
-#         for k, d in enumerate(dets):
-#             shape = sp(img, d)
-#             face_descriptor = facerec.compute_face_descriptor(img, shape)
-#             d_test = numpy.array(face_descriptor)
-#
-#
-#             for i in descriptors:  # 计算距离
-#                 dist_ = numpy.linalg.norm(i - d_test)
-#                 dist.append(dist_)
-#                 Range += dist_
-#
-#         print("测试图与训练数据的平均相似度",1-(Range/len(dist)))
-#
-#         # 训练集人物和距离组成一个字典
-#         c_d = dict(zip(candidate, dist))
-#         cd_sorted = sorted(c_d.items(), key=lambda d:d[1])
-#
-#         print("识别到的相似度最高的人物:{} ,相似度为{}".format(cd_sorted[0][0], 1-cd_sorted[0][1]))
-#         print(cd_sorted)    # 距离值越小，相似度越高
-
 
 class FeatureMatch:
     def __init__(self, model_path, path_to_data, path_to_query):
@@ -98,9 +32,9 @@ class FeatureMatch:
 
     def _Run(self):
         # load model
-        model = torchvision.models.detection.maskrcnn_resnet50_fpn(pretrained=False, pretrained_backbone=False)
+        model = torchvision.models.resnet50(pretrained=False)
         model.load_state_dict(
-              torch.load(self.rec_model_path), strict=False)
+              torch.load(self.rec_model_path), strict=True)
         model.eval()
 
         # build dataloader
@@ -152,32 +86,13 @@ class FeatureMatch:
 
                 # extract fature
                 output = resnet50_feature_extractor(x)
-                print(output[0]['boxes'].shape)
+                print(output.shape)
 
                 # 计算余弦相似度，计算哪个维度上的相似度？？
-                similarity = torch.cosine_similarity(q_feature[0]['boxes'], output[0]['boxes'], dim=0)
+                similarity = torch.cosine_similarity(q_feature, output, dim=1)
+                print(similarity)       # 多张图片则可能有多次输出
 
-                for index in range(output[0]['boxes'].shape):
-                    if similarity[index] > self.threshold:
-                        result.append(image_names[count * BATCH_SIZE + index][0])
-                count += 1
-
-        # print the results
-        for r in result:
-            print(r)
-
-
-        similarity = 0
         return similarity, result
-
-
-    # from shutil import copyfile
-    # import os
-    # os.makedirs(target_dir, exist_ok=True)
-    # for r in result:
-    #     copyfile(r, target_dir+'/'+r.split('/')[-1])
-
-
 
 
 if __name__ == '__main__':
@@ -190,5 +105,3 @@ if __name__ == '__main__':
 
     Func = FeatureMatch(model_path, path_to_data, path2Query)
     Func._Run()
-
-
