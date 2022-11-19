0d484bfdc0e2f1d46b801326aaa63e07af688dca
PengYu <zaixiadouing@qq.com>
2022-11-20 02:59:33 +0800

    -

--- Methods_Demo/DelDocSpace_Demo.py
+++ Methods_Demo/DelDocSpace_Demo.py
@@ -4,8 +4,8 @@
 # ecoding=utf-8
 
 
-outfile = open(r"output.txt", 'a+',encoding="utf8")
-with open(r"1.txt", 'r', encoding="utf8") as f:
+outfile = open(r"../output.txt", 'a+', encoding="utf8")
+with open(r"../1.txt", 'r', encoding="utf8") as f:
     for eachline in f.readlines():
         # 去掉文本行里面的空格、\t、数字（其他有要去除的也可以放到' \t1234567890'里面）
         lines = filter(lambda ch: ch not in ' ', eachline)      # __class = filter
