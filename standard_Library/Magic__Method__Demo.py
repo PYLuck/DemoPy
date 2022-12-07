282623e214bea440c919ffbb10a04b0535deae74
blackeyy <zaixiadouing@qq.com>
2022-12-07 17:18:00 +0800

    setattr()

--- standard_Library/Magic__Method__Demo.py
+++ standard_Library/Magic__Method__Demo.py
@@ -1,4 +1,4 @@
-# python常用的魔法方法使用 __init__(), __setattr__(),
+# python常用的魔法方法使用 __init__(), __setattr__(), getattr()
 # 介绍详见 Manual Doc/Magic Methods.md
 
 # Demo-1
@@ -15,6 +15,7 @@ class Tag:
         print('调用setitem, 修改')
         self.change[key] = value
 
+
 # Demo-2
 class Dict(dict):
     __setattr__ = dict.__setitem__
@@ -23,16 +24,13 @@ class Dict(dict):
     # __getattr__ = lambda d, k: d.get(k, '')  # dict.k  ==>  dict.get(k,default)
 
 
-
-
-
 if __name__ == '__main__':
     pass
     # ==================== Magic Method : __setattr__() ============================
     '''Demo-1: 获取与修改'''
     a = Tag()
     print(a['php'])
-    a['php'] = 'PHP is not a good language'     # 传参的同时，调用 __getitem__(self, item)
+    a['php'] = 'PHP is not a good language'  # 传参的同时，调用 __getitem__(self, item)
     print(a['php'], 'Demo-1\n')
 
     '''Demo-2:实现字典转对象，即字典可以用dict.key获取 值'''
@@ -40,4 +38,15 @@ if __name__ == '__main__':
     D.c = '233'
     print(D.c, 'Demo-2\n')
 
-    # ==================================================================
+    # ======================内置函数: getattr(), setattr()============================================
+    '''getattr() 函数用于返回一个 对象属性 值'''
+    class New_Obj(object): name = 'py'
+    obj = New_Obj();    obj.y_name = 'pyl'
+    res = getattr(obj, 'name')        # 获取属性name的值
+    res1 = getattr(obj, 'y_name')        # 属性不存在时报错
+    res_err = getattr(obj, 'you', '属性不存在')        # 属性不存在时报错,或设置默认值''
+    print(res, res1, res_err)
+
+    '''setattr() 设置属性值，该属性不一定是存在的'''
+    setattr(obj, 'age', 20)         # return None
+    print(obj.age)
